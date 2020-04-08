24baa5d733064b09a8374eb2ddefae4ac5a61e39
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-04-09 01:47:55 +0300

    created login page (frontend)

--- kreem-website/routes/web.php
+++ kreem-website/routes/web.php
@@ -13,11 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/schedule', 'ScheduleController@index');
+Route::get('/loginpage', function () {
+    return view('login');
+});
