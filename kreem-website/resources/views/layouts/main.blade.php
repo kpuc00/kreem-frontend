89fbfc50bac8f0058b1d48ff086f479ff1558c8c
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-04-15 20:04:20 +0300

    fixed the logo in the login page

--- kreem-website/resources/views/layouts/main.blade.php
+++ kreem-website/resources/views/layouts/main.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Media Bazaar')</title>
     
@@ -27,18 +27,20 @@
             
             <div class="col-5 m-0 p-0">
                 <div class="leftside-wrapper">
+                    <img class="logo-small m-0" src="images/Logo-small.svg">
+                <div class="col-5 position-fixed">
                     <h3 class="m-0 pt-5">Media Bazaar</h3>
                     <h4 class="m-0 pt-3 pb-5">Employee view</h4>
                     <h5 class="text m-0 pt-5">The place where employees can check their stats and information</h5>
+                </div>
 
-                    <img class="logo-small m-0" src="images/Logo-small.svg">
                 </div>
                 </div>
                 
                 
 
             <div class="col-7 m-0 p-0">
-                <div class="rightside-wrapper pl-5 pr-5">
+                <div class="rightside-wrapper">
                 @yield('content')
                 </div>                
             </div>
