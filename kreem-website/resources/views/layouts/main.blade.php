d1a740e90e6ea68398d6690c644e17353b864380
Kristiyan Strahilov <kristiyanstrahilov00@gmail.com>
2020-04-16 19:31:18 +0300

    login page fixed tags in css

--- kreem-website/resources/views/layouts/main.blade.php
+++ kreem-website/resources/views/layouts/main.blade.php
@@ -20,31 +20,29 @@
     
 </head>
 <body>
-
-    <div class="container-fluid m-0 p-0">
     
+    <div class="container-fluid m-0 p-0">
+        
         <div class="row m-0">
             
             <div class="col-5 m-0 p-0">
                 <div class="leftside-wrapper">
                     <img class="logo-small m-0" src="images/Logo-small.svg">
-                <div class="col-5 position-fixed">
-                    <h3 class="m-0 pt-5">Media Bazaar</h3>
-                    <h4 class="m-0 pt-3 pb-5">Employee view</h4>
-                    <h5 class="text m-0 pt-5">The place where employees can check their stats and information</h5>
+                    <div class="col-5 position-fixed">
+                        <h3 class="m-0 pt-5">Media Bazaar</h3>
+                        <h4 class="m-0 pt-3 pb-5">Employee view</h4>
+                        <h5 class="text m-0 pt-5">The place where employees can check their stats and information</h5>
+                    </div>
+                    
                 </div>
-
-                </div>
-                </div>
-                
-                
-
+            </div>
+            
             <div class="col-7 m-0 p-0">
                 <div class="rightside-wrapper">
                 @yield('content')
                 </div>                
             </div>
-    
+                
         </div>
     
     </div>
