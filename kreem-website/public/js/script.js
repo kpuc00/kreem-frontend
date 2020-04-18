f77c1e16a9573a7e49d7f7a3f09cd416feca2c4e
Mihail Bondarenco <441044@student.fontys.nl>
2020-04-18 19:41:17 +0200

    feat(calendar) set up icons and call in functionoality

--- kreem-website/public/js/script.js
+++ kreem-website/public/js/script.js
@@ -9,6 +9,11 @@ let months = ["January", "February", "March", "April", "May", "June", "July", "A
 let monthAndYear = document.getElementById("monthAndYear");
 let calendar = {};
 
+let shiftIcons = {
+    Morning : '🌄',
+    Noon : '🌞',
+    Night : '🌙',
+}
 
 showCalendar(currentMonth, currentYear);
 
@@ -64,6 +69,9 @@ function showCalendar(month, year) {
         calendar[dateKey] = currentCell;
 
         currentCell.textContent = currentDay.getDate().toString();
+        currentCell.setAttribute('data-date', dateToISO(currentDay));
+        currentCell.setAttribute('data-isShiftAssigned', 'false');
+
         row.appendChild(currentCell);
         if(currentDay.getDay() === 0){
             tbl.appendChild(row);
@@ -88,16 +96,69 @@ async function refreshCalendarFromAPI(date){
     const json = await response.json();
 
 
-    for(let i in json){
-        let shift = json[i]
+    json.forEach( shift => {
         let thisCell = calendar[shift.date];
 
         if(thisCell === undefined)
-            continue;
+            return;
 
-        thisCell.innerHTML = shift.type.name;
-    }
+        setCellForShift(thisCell, shift);
+    });
+}
 
+function assignedShift_MouseOver ( event ) {
+    let icon = createIcon('😷');
+    icon.addEventListener('click', callIn);
+    event.target.innerHTML = '';
+    event.target.appendChild(icon);
 
 }
 
+function setCellForShift(cell, shift){
+    cell.innerHTML = '';
+    cell.setAttribute('data-isShiftAssigned', 'true');
+    cell.setAttribute('data-shiftId', shift.id);
+
+    let icon;
+    if(shift.callIn === null){
+        icon = createIcon(shiftIcons[shift.type.name]);
+        icon.addEventListener("mouseenter", assignedShift_MouseOver);
+        icon.addEventListener("mouseleave", () => setCellForShift(cell, shift) );
+
+    }else{
+        icon = createIcon('😷');
+        icon.setAttribute('title', 'You already called in sick');
+        icon.setAttribute('data-toggle', 'tooltip');
+        $(icon).tooltip();
+    }
+
+    cell.appendChild(icon);
+
+}
+
+
+function createIcon(text){
+    let icon = document.createElement('h2');
+    icon.textContent = text;
+    icon.style.cursor = 'pointer';
+    return icon;
+}
+
+
+async function callIn(event){
+    let shiftId = event.target.parentElement.getAttribute('data-shiftId')
+
+    let reason = prompt('Give reason (Personal, Sick, Other)');
+
+    let response = await fetch(`/json/shifts/${shiftId}/callins`, {
+        method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body:JSON.stringify({reason})
+    });
+
+    if(response.ok){
+        alert('You have called in sick');
+    }else{
+        alert('Something went wrong, please try again');
+    }
+}
